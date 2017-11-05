6cbbc45f5e63f5a6bbc7e8607189a25ca3983d23
Tiseno <math.lindgren@gmail.com>
2017-11-05 14:33:02 +0100

    Windows support, cleanup and authentication error handling

--- submit.py
+++ submit.py
@@ -6,6 +6,10 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+from colorama import init, deinit
+
+init()
+
 # Python 2/3 compatibility
 if sys.version_info[0] >= 3:
     import configparser
@@ -15,7 +19,6 @@ else:
 
 
 _HEADERS = {'User-Agent': 'kattis-cli-submit'}
-_DEFAULT_CONFIG = '/usr/local/etc/.kattisrc'
 _LANGUAGE_GUESS = {
     '.java': 'Java',
     '.c': 'C',
@@ -38,8 +41,9 @@ _LANGUAGE_GUESS = {
     '.rb': 'Ruby'
 }
 _GUESS_MAINCLASS = {'Java', 'Python'}
-_PASSED_TEST_SIGN = u"\u2713"
-
+_PASSED_TEST_SIGN = u'\u2713'
+if os.name == 'nt':
+    _PASSED_TEST_SIGN = 'P'
 
 parser = argparse.ArgumentParser(description='Submit a solution to Kattis')
 
@@ -57,9 +61,6 @@ args = parser.parse_args()
 files = args.files
 
 cfg = configparser.ConfigParser()
-
-if os.path.exists(_DEFAULT_CONFIG):
-    cfg.read(_DEFAULT_CONFIG)
 
 if not cfg.read([os.path.join(os.getenv('HOME'), '.kattisrc'),
                  os.path.join(os.path.dirname(sys.argv[0]), '.kattisrc')]):
@@ -98,7 +99,6 @@ login_args = {
     'token': token
     }
 
-
 login_reply = None
 try:
     login_reply = requests.post(login_url, data=login_args, headers=_HEADERS)
@@ -106,9 +106,13 @@ except Exception as e:
     print('Connection to {0} failed.'.format(login_url))
     sys.exit(1)
 
+if login_reply.status_code != 200:
+    print(login_reply.status_code)
+    print('Could not authenticate')
+    print(login_reply.content)
+    sys.exit(1)
 
 # Guess language and problem
-
 problem, ext = os.path.splitext(os.path.basename(args.files[0]))
 language = _LANGUAGE_GUESS.get(ext, None)
 mainclass = problem if language in _GUESS_MAINCLASS else None
@@ -117,12 +121,12 @@ if args.problem:
     problem = args.problem
 if args.language:
     language = args.language
-elif language == "Python":
+elif language == 'Python':
     python_version = str(sys.version_info[0])
-    language = "Python " + python_version
+    language = 'Python ' + python_version
 
 if language is None:
-    print("I failed to guess the language and you didn't provide one for me. I'm going back to bed")
+    print('I failed to guess the language and you didn\'t provide one for me. I\'m going back to bed')
     exit(1)
 
 confirm_submission = not args.force
@@ -133,9 +137,9 @@ if confirm_submission:
     print('Language:', language)
     print('Files:', ', '.join(files))
 
-    confirmation = input("Submit problem? (Y/y) ")
+    confirmation = input('Submit problem? (Y/y) ')
 
-    if confirmation not in "yY":
+    if confirmation not in 'yY':
         exit()
 
 # Submit
@@ -166,17 +170,17 @@ except Exception as e:
 response_content = response.content.decode('utf-8').replace('<br />', '\n')
 print(response_content)
 submission_id = response_content.split()[4][:-1]
-print(base_url + "/submissions/" + submission_id)
+print(base_url + '/submissions/' + submission_id)
 # Soup it up TODO improve
 # if confirm_submission:
-#     confirmation = input("Get results? (Y/y) ")
-#     if confirmation not in "Yy":
+#     confirmation = input('Get results? (Y/y) ')
+#     if confirmation not in 'Yy':
 #         exit()
 
 while True:
     try:
         login_reply = requests.post(login_url, data=login_args, headers=_HEADERS)
-        response = requests.get(base_url + "/submissions/" + submission_id, cookies=login_reply.cookies,
+        response = requests.get(base_url + '/submissions/' + submission_id, cookies=login_reply.cookies,
             headers=_HEADERS)
     except Exception as e:
         print('Connection to {0} failed.'.format(submit_url))
@@ -188,41 +192,43 @@ while True:
     submission_status = soup.select('.status')[0]
     passed_tests = 0
 
-    sys.stdout.write("[")
+    sys.stdout.write('[')
 
     for case in test_cases:
         classes = case.attrs.get('class', [])
-        if "accepted" in classes:
+        if 'accepted' in classes:
             passed_tests += 1
-            sys.stdout.write("\033[92m")
-            sys.stdout.write(_PASSED_TEST_SIGN + " ")
-        elif "rejected" in classes:
-            sys.stdout.write("\033[91m")
-            sys.stdout.write("X ")
+            sys.stdout.write('\033[92m')
+            sys.stdout.write(_PASSED_TEST_SIGN + ' ')
+        elif 'rejected' in classes:
+            sys.stdout.write('\033[91m')
+            sys.stdout.write('X ')
         else:
-            sys.stdout.write("\033[39m")
-            sys.stdout.write("O ")
+            sys.stdout.write('\033[39m')
+            sys.stdout.write('O ')
 
-    sys.stdout.write(" | " + str(passed_tests) + "/" + str(len(test_cases)))
-    sys.stdout.write("\033[39m] " + str(submission_status.text))
+    sys.stdout.write(' | ' + str(passed_tests) + '/' + str(len(test_cases)))
+    sys.stdout.write('\033[39m] ' + str(submission_status.text))
     sys.stdout.flush()
 
     status = submission_status.attrs.get('class', [])
     if 'accepted' in status or 'rejected' in status:
         if 'rejected' in status:
-            sys.stdout.write("\033[91m")
+            sys.stdout.write('\033[91m')
             print()
-            print("Oh no! Your submission resulted in a", str(submission_status.text))
-            sys.stdout.write("\033[39m")
+            print('Oh no! Your submission resulted in a', str(submission_status.text))
+            sys.stdout.write('\033[39m')
             print()
             compileroutput = soup.select('.extrainfo')
             if compileroutput:
-                print("Compiler output: ")
+                print('Compiler output: ')
                 print(compileroutput[0].find('pre').text)
-                print("")
+                print()
         else:
             tottime = soup.select('.runtime.middle')[0].text
-            print(" Time: " + tottime)
+            print(' Time: ' + tottime)
         exit(1)
     sys.stdout.write('\r')
     time.sleep(1)
+
+deinit()
